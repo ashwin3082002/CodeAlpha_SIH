ed0802c88dc47835ae955491248dafb98c628aea
Ashwin <ashwin3082002@gmail.com>
2022-08-14 23:19:37 +0530

    removed email sending in index page

--- sipportal/views.py
+++ sipportal/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 from util import func
 def index(request):
-    print(func.sendotp("sqlmy321@gmail.com"))
     return render(request,'index.html')
 # Added by Laavesh
 def about(request):
