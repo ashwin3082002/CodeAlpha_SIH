9964b6db1d6ad47ce5b77596a3aecd9f65fe03b7
Ashwin <ashwin3082002@gmail.com>
2022-08-26 04:10:32 +0530

    bank details api

--- sip_api/views.py
+++ sip_api/views.py
@@ -1,7 +1,7 @@
 from unittest import result
 from sip_api.serializers import StudentSerializer, ApikeySerializer
 from rest_framework.views import APIView
-from sip_db.models import degree, institution_detail, student_detail
+from sip_db.models import account_detail, degree, institution_detail, student_detail
 from django.http import JsonResponse
 from sip_db.models import student_detail, api_details
 # Create your views here.
@@ -77,7 +77,6 @@ class stu_detail(APIView):
         if api_key == None:
             return JsonResponse({'status':'Permission Denied',"message":"You are not authorized to use this API"})
         else:
-            search_details = student_detail.objects.filter(sid = sid).values()
             search_details_api = api_details.objects.filter(api_key=api_key).values()
             try:
                 perm = search_details_api[0]['permissions']
@@ -96,5 +95,33 @@ class stu_detail(APIView):
                         return JsonResponse(result_dict)
                     else:
                         return JsonResponse({"Status":"Failed","Message":"SID Doesn't Exists"})
+            else:
+                return JsonResponse({'status':'Permission Denied',"message":"You Don't Have permission to query this API"})
+
+class bank_account(APIView):
+    def get(self,request,*args,**kwargs):
+        sid = self.request.query_params.get("sid")
+        api_key = self.request.query_params.get("api_key")
+        if api_key == None:
+            return JsonResponse({'status':'Permission Denied',"message":"You are not authorized to use this API"})
+        else:
+            search_details_api = api_details.objects.filter(api_key=api_key).values()
+            try:
+                perm = search_details_api[0]['permissions']
+            except:
+                return JsonResponse({'status':'Permission Denied',"message":"You are not authorized to use this API"})
+            if perm == 'bank':
+                serializer1 = ApikeySerializer(search_details_api, many = True)
+                if serializer1.data == []:
+                    return JsonResponse({'status':'Permission Denied',"message":"You are not authorized to use this API"})
+                elif api_key == serializer1.data[0]['api_key']:
+                    search_details = account_detail.objects.filter(sid=sid).values()
+                    if search_details:
+                        search_details=search_details[0]
+                        result_dict = {"Status":"Success"}
+                        result_dict['data'] = search_details
+                        return JsonResponse(result_dict)
+                    else:
+                        return JsonResponse({"Status":"Failed","Message":"Bank Details Does'nt Exists for the SID"})
             else:
                 return JsonResponse({'status':'Permission Denied',"message":"You Don't Have permission to query this API"})
