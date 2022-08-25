fa0435000709c4703b1179bf3efd80e76b7202df
Ashwin <ashwin3082002@gmail.com>
2022-08-25 11:47:33 +0530

    mithun somberi js

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,28 @@ function navbtn() {
     x.style.display = "none";
   }
 }
+
+function profile() {
+  var x = document.getElementById("profile");
+  var y = document.getElementById("create-profile");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+
+function createprofile() {
+  var x = document.getElementById("profile");
+  var y = document.getElementById("create-profile");
+  if (y.style.display === "none") {
+    x.style.display = "none";
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
